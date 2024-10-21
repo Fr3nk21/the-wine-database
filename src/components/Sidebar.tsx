05e9595527f97f0b2326bf24c6873e4af357cccf
Fr3nk <bugugnolifrancesco@gmail.com>
2024-10-22 08:15:08 +1100

    Sidebar Home Link

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,12 +1,15 @@
 function Sidebar() {
   return (
-    <div className="bg-red-500 flex flex-col p-20">
+    <div className="flex flex-col p-20 bg-red-500">
       <div>
         <img />
       </div>
       <div>
         <h2 className="pb-6">Title of the Dashboard</h2>
         <ul>
+          <li>
+            <a href="http://localhost:5173/">Home</a>
+          </li>
           <li>
             <a href="http://localhost:5173/dashboard">Dashboard</a>
           </li>
@@ -19,7 +22,7 @@ function Sidebar() {
         <h3>Title</h3>
       </div>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
