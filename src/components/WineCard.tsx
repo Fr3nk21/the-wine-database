d061e63c83026732a25289da135b29e960378693
Fr3nk <bugugnolifrancesco@gmail.com>
2024-10-27 08:39:54 +1100

    Adjust wine card layout and style

--- src/components/WineCard.tsx
+++ src/components/WineCard.tsx
@@ -14,9 +14,9 @@ const WineCard: FC<WineCardProps> = ({
   description,
 }) => {
   return (
-    <div className="flex flex-row overflow-hidden rounded-lg bg-white p-6 shadow-md">
+    <div className="flex flex-row p-6 overflow-hidden bg-white rounded-lg shadow-md">
       {/* Left content column */}
-      <div className="relative z-10">
+      <div className="relative z-10 w-1/3">
         <div className="mb-4">
           <h3 className="mb-3 text-3xl font-bold">{name}</h3>
           <span
@@ -27,11 +27,11 @@ const WineCard: FC<WineCardProps> = ({
         </div>
 
         <div className="mb-3">
-          <p className="text-gray-600">Origin: {origin}</p>
+          <p className="text-gray-600">🌎 Origin: {origin}</p>
         </div>
 
         <div className="mb-3">
-          <p className="font-medium">Characteristics:</p>
+          <p className="font-medium">🍷 Characteristics:</p>
           <ul className="text-sm text-gray-600">
             <li>Body: {characteristics.body}</li>
             <li>Tannins: {characteristics.tannins}</li>
@@ -40,32 +40,32 @@ const WineCard: FC<WineCardProps> = ({
         </div>
 
         <div className="mb-3">
-          <p className="font-medium">Flavors:</p>
+          <p className="font-medium">😋 Flavors:</p>
           <p className="text-sm text-gray-600">
             {characteristics.flavors.join(', ')}
           </p>
         </div>
 
         <div className="mb-3">
-          <p className="fon-medium">Food Pairings:</p>
+          <p className="fon-medium">🍖 Food Pairings:</p>
           <p className="text-sm text-gray-600">{foodPairings.join(', ')}</p>
         </div>
 
-        <div className="mt-4 flex items-center justify-between">
+        <div className="flex items-center justify-between mt-4">
           <span className="text-lg font-bold">${price}</span>
         </div>
       </div>
 
       {/* Central colimn with wine description */}
-      <div className="items center w-1/3">
-        <p>dsds{description}</p>
+      <div className="flex items-center justify-center w-1/3 text-center align-middle">
+        <p className="text-sm">{description}</p>
       </div>
 
       {/* Right column with bottle and flag */}
-      <div className="relative ml-auto">
+      <div className="relative flex items-center justify-center w-1/3 ml-auto align-middle">
         <img
           src={origin === 'Italy' ? italy : france}
-          className="absolute -right-12 -top-0 z-0 max-w-96 -rotate-12 scale-150 object-cover opacity-10"
+          className="absolute z-0 object-cover scale-150 -right-12 -top-0 max-w-96 -rotate-12 opacity-10"
         />
         <img src={bottle} className="relative z-10 max-w-60" />
       </div>
